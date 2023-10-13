adde6a1ff664bffea363e5224e6caac8368a9b5b
Veronica Stork <vers@sitecore.net>
2023-10-13 14:29:55 -0400

    Makes program ONLY extract meta tags and title tag

--- pathfinder.js
+++ pathfinder.js
@@ -23,7 +23,7 @@ const processCode = (code) => {
       // }
       {
         role: 'user',
-        content: `Can you look at the following source code: ${code}, then find attributes that represent the title, description, image URL, and subtitle along with xpath expressions for extracting those attributes?`
+        content: `Can you look at the following array of HTML tags: ${code}, then find attributes that represent the title, description, image URL, site URL, locale, and subtitle along with xpath expressions for extracting those attributes? If an attribute does not appear to be in the HTML tags, do not return an object for it.`
       },
     ],
     "temperature": 0,
@@ -37,7 +37,7 @@ const processCode = (code) => {
     .then((response) => response.json())
     .then((data) => {
       const tempArr = data.choices[0].message.content
-      console.log(tempArr);
+      console.log(tempArr)
     })
     .catch((error) => {
       console.error('Error:', error);
@@ -59,13 +59,22 @@ const processCode = (code) => {
 // }
 
 // This is just another version of the "getSourceCode" function that gets JUST the head.
-async function extractHeadFromUrl(url) {  
+async function extractMetaAndTitleFromUrl(url) {  
   const response = await axios.get(url);  
   const html = response.data;  
   const dom = new JSDOM(html);  
-  const head = dom.window.document.head;  
-  return head.outerHTML;  
+  const metaTags = dom.window.document.querySelectorAll('meta');  
+
+  const metaStrings = [];  
+for (let i = 0; i < metaTags.length; i++) {  
+  metaStrings.push(metaTags[i].outerHTML)
 }  
+const title = dom.window.document.querySelector('title');
+metaStrings.push(title.outerHTML)
+return metaStrings;
+}  
+
+// extractMetaAndTitleFromUrl('https://www.pbs.org/parents/halloween')
 
 function chunkSourceCode(sourceCode) {
   const textSegments = [];
@@ -81,16 +90,17 @@ function chunkSourceCode(sourceCode) {
 
 async function pathFinder() {
 
-  const sourceCode = await extractHeadFromUrl('https://www.pbs.org/parents/halloween');
-  const chunkedCode = chunkSourceCode(sourceCode);
+  const sourceCode = await extractMetaAndTitleFromUrl('https://www.pbs.org/parents/halloween');
+  // const chunkedCode = chunkSourceCode(sourceCode);
 
-  const aiReponsePromise = chunkedCode.map(code => processCode(code))
+  // const aiReponsePromise = chunkedCode.map(code => processCode(code))
   
   console.log('Fetching AI reponses...')
   
-  const responses = await Promise.all(aiReponsePromise)
-
-  responses.forEach(resp => console.log(resp))
+  const response = await processCode(sourceCode);
+  // const responses = await Promise.all(aiReponsePromise)
+  console.log(response);
+  // responses.forEach(resp => console.log(resp))
 }
 
 pathFinder();
