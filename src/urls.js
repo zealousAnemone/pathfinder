1b09b327517a300d18d14c9926bbe83c64e006b7
sonia-sitecore <sona@sitecore.net>
2023-10-19 01:01:11 -0400

    new UI

---
+++ src/urls.js
@@ -0,0 +1,6 @@
+export default [
+    "https://torontopubliclibrary.ca/books-video-music/books/",
+    "https://www.oshawa.ca/en/parks-recreation-and-culture/bright-and-merry-market.aspx",
+    "https://doc.sitecore.com/search/en/users/search-user-guide/sources.html",
+    "https://veronica-stork.com"
+]
